3c3a1eb6f9cb16cf2bf7e89064ee56a84661776b
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-18 21:52:07 +0200

    test: should 50 + 70 return 120: passed

--- src/operations/operations.test.tsx
+++ src/operations/operations.test.tsx
@@ -1,5 +1,9 @@
-import addition from "./addition/addition";
+import addition from './addition/addition';
 
 test('should return 2 if numbers are 1 and 1', () => {
-    expect(addition(1, 1)).toBe(2)
-})
+  expect(addition(1, 1)).toBe(2);
+});
+
+test('should return 120 if numbers are 50 and 70', () => {
+  expect(addition(50, 70)).toBe(120);
+});
