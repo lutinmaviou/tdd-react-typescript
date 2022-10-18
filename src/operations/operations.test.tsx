9f9607223d12db4bd63f3e37854ff4d68d29a2c9
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-18 22:08:13 +0200

    test: should 4 - 2 return 2: failed

--- src/operations/operations.test.tsx
+++ src/operations/operations.test.tsx
@@ -1,4 +1,5 @@
-import addition from './addition/addition';
+import addition from './addition';
+import substraction from './substraction';
 
 test('should return 2 if numbers are 1 and 1', () => {
   expect(addition(1, 1)).toBe(2);
@@ -6,4 +7,8 @@ test('should return 2 if numbers are 1 and 1', () => {
 
 test('should return 120 if numbers are 50 and 70', () => {
   expect(addition(50, 70)).toBe(120);
+});
+
+test('should return 2 if numbers are 4 and 2', () => {
+  expect(substraction(4, 2)).toBe(2);
 });
---
+++ src/operations/substraction.tsx
@@ -0,0 +1,4 @@
+const substraction = (nb1: number, nb2: number): number => {
+};
+
+export default substraction;
