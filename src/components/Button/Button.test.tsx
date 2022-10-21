5a5526e7c0c7c0c40440bac1a3daa1a2df374d7c
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-21 19:48:03 +0200

    refactor: reorganize tests files properly

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,8 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Button from './Button';
 
 describe('<Button component', () => {
-  test('Should render component with class equals and value =', () => {
+  test('Should render with class "equals" and value "="', () => {
     render(<Button className="equals" value="-" />);
+  });
+});
+
+describe('button element', () => {
+  test('should render with className "addition"', () => {
+    render(<Button className="addition" value="" />);
+    const button = screen.getByRole('button');
+    expect(button).toHaveClass('addition');
   });
 });
--- src/operations/operations.test.tsx
+++ src/operations/operations.test.tsx
@@ -1,30 +1,19 @@
 import operations from './operations';
-import { screen } from '@testing-library/dom';
-import { render } from '@testing-library/react';
-import Button from '../components/Button/Button';
 
 describe('operations type', () => {
-  test('should return 2 if numbers are 1 and 1', () => {
+  test('should return 2 if numbers are 1 and 1, and type is "addition"', () => {
     expect(operations(1, 1, 'addition')).toBe(2);
   });
 
-  test('should return 2 if numbers are 4 and 2', () => {
+  test('should return 2 if numbers are 4 and 2, and type is "substraction"', () => {
     expect(operations(4, 2, 'substraction')).toBe(2);
   });
 
-  test('should return 6 if numbers are 2 and 3', () => {
+  test('should return 6 if numbers are 2 and 3, and type is "multiplication"', () => {
     expect(operations(2, 3, 'multiplication')).toBe(6);
   });
 
-  test('should return 3 if numbers are 12 and 4', () => {
+  test('should return 3 if numbers are 12 and 4, and type is "division"', () => {
     expect(operations(12, 4, 'division')).toBe(3);
-  });
-});
-
-describe('button component', () => {
-  test('should render button component with addition className', () => {
-    render(<Button className="addition" value="" />);
-    const button = screen.getByRole('button');
-    expect(button).toHaveClass('addition');
   });
 });
