ed43b6ce3c641b4320684551352b32a1bf52fdf5
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-23 18:26:44 +0200

    test: click on button element, failed

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,16 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
-describe('<Button component', () => {
+describe('Button component', () => {
   test('Should render with class "equals" and value "="', () => {
-    render(<Button className="equals" value="-" />);
+    render(<Button className="equals" value="-" title="" />);
   });
 });
 
 describe('button element', () => {
   test('should render with className "addition"', () => {
-    render(<Button className="addition" value="" />);
+    render(<Button className="addition" value="" title="" />);
     const button = screen.getByRole('button');
     expect(button).toHaveClass('addition');
+  });
+});
+
+describe('handles onClick', () => {
+  test('return something', () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick} title="addition" className="signs" value="+" />);
+    const buttonElt = screen.getByRole('button');
+    fireEvent.click(buttonElt);
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
